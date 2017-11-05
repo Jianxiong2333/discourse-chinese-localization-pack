bb836acfdbd9ea626f14bdfb1a2211ba4e2c9043
winkidney <winkidney@gmail.com>
2017-11-05 02:16:54 -0800

    Update: Use new iframe for youku

--- lib/onebox/youku.rb
+++ lib/onebox/youku.rb
@@ -1,13 +1,38 @@
-module ::YoukuOneboxOverride
-  def to_html
-    if SiteSetting.zh_l10n_http_onebox_override
-      "<iframe width='480' height='270' src='http://player.youku.com/embed/#{video_id}' frameborder='0' allowfullscreen></iframe>"
-    else
-      super
+module Onebox
+  module Engine
+    class YoukuOnebox
+      include Engine
+      include HTML
+
+      matches_regexp(/^(https?:\/\/)?([\da-z\.-]+)(youku.com\/)(.)+\/?$/)
+
+      # Try to get the video ID. Works for URLs of the form:
+      # * http://v.youku.com/v_show/id_XNjM3MzAxNzc2.html
+      # * http://v.youku.com/v_show/id_XMTQ5MjgyMjMyOA==.html?from=y1.3-tech-index3-232-10183.89969-89963.3-1
+      def video_id
+        match = uri.path.match(/\/v_show\/id_([a-zA-Z0-9_=\-]+)(\.html)?.*/)
+        return match[1] if match && match[1]
+
+        nil
+      rescue
+        return nil
+      end
+
+      def to_html
+        "<embed width='570' height='360' src='https://players.youku.com/player.php/sid/#{video_id}/v.swf' wmode='transparent' allowFullScreen='true' quality='high' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>"
+      end
+
+      def placeholder_html
+        to_html
+      end
+
+      private
+
+      # Note: May throw! Make sure to rescue.
+      def uri
+        @_uri ||= URI(@url)
+      end
+
     end
   end
-end
-
-Onebox::Engine::YoukuOnebox.class_eval do
-  prepend ::YoukuOneboxOverride
 end
